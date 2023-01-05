39ec7602705487a431d7368c5cd3a7dfbbd740fb
Tatiane Yukari <yukari.kawakami@hotmail.com>
2023-01-05 01:46:19 -0300

    update words to learn

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
-
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 def next_card():
     global current_card, flip_time
@@ -18,10 +24,18 @@ def next_card():
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     flip_time = window.after(3000, func=flip_card)
 
+
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_bg, image=card_back)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
@@ -29,7 +43,6 @@ window.title("Flash Cards")
 window.config(padx=20, pady=20, bg=BACKGROUND_COLOR)
 
 flip_time = window.after(3000, func=flip_card)
-
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front = PhotoImage(file="images/card_front.png")
@@ -41,13 +54,11 @@ card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "ital
 card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
-button_right = Button(image=card_right, bg=BACKGROUND_COLOR, highlightthickness=0, border=0, command=next_card)
+button_right = Button(image=card_right, bg=BACKGROUND_COLOR, highlightthickness=0, border=0, command=is_known)
 button_right.grid(column=1, row=1)
 button_wrong = Button(image=card_wrong, bg=BACKGROUND_COLOR, highlightthickness=0, border=0, command=next_card)
 button_wrong.grid(column=0, row=1)
 
-
 next_card()
-
 
 window.mainloop()
